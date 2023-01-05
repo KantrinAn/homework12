b013ef9ee55ec8faa5f647c70aecaed9654ef89a
KatrinAn <anpipinakaka@gmail.com>
2023-01-06 00:49:04 +0200

    deleted space

--- src/Person.java
+++ src/Person.java
@@ -14,25 +14,18 @@ public class Person {
     person1.city = "New York";
     person1.phone= "2936729462846";
     person1.personInfo(person1);
-
-
-
     Person person2 = new Person();
         person2.firstName = "Jackie";
         person2.lastName ="Chan";
         person2.city = "Shanghai";
         person2.phone = "12312412412";
         person2.personInfo(person2);
-
     Person person3 = new Person();
      person3.firstName = "Sherlock";
      person3.lastName = "Holmes";
      person3.city = "London";
      person3.phone = "37742123513";
      person3.personInfo(person3);
-
-
-
          }
 
     }
